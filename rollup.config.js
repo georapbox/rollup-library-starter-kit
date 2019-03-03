f3c541f6fe396e4c7c6a287dabcf189e231ea485
georapbox <georapbox@gmail.com>
2019-03-03 23:04:34 +0200

    create rollup configuration for producting minified umd build

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 // import resolve from 'rollup-plugin-node-resolve';
 // import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 const libraryName = 'Library'; // Change with your library's name
@@ -16,46 +17,76 @@ const banner = `/*!
  * @license ${pkg.license}
  */`;
 
-export default [
-  // Browser-friendly UMD build
-  {
-    input: 'src/index.js',
-    output: {
-      banner,
-      name: libraryName,
-      file: pkg.browser,
-      format: 'umd'
-    },
-    plugins: [
-      // Uncomment the following 2 lines if your library has external dependencies
-      // resolve(), // teach Rollup how to find external modules
-      // commonjs(), // so Rollup can convert external modules to an ES module
-      babel({
-        exclude: ['node_modules/**']
-      })
-    ]
-  },
-
-  // CommonJS (for Node) and ES module (for bundlers) build
-  {
-    input: 'src/index.js',
-    external: [], // indicate which modules should be treated as external
-    output: [
-      {
+export default commandLineArgs => {
+  const configs = [
+    // UMD Development
+    {
+      input: 'src/index.js',
+      output: {
         banner,
-        file: pkg.main,
-        format: 'cjs'
+        name: libraryName,
+        file: pkg.browser,
+        format: 'umd'
       },
-      {
+      plugins: [
+        // Uncomment the following 2 lines if your library has external dependencies
+        // resolve(), // teach Rollup how to find external modules
+        // commonjs(), // so Rollup can convert external modules to an ES module
+        babel({
+          exclude: ['node_modules/**']
+        })
+      ]
+    },
+
+    // CommonJS (for Node) and ES module (for bundlers) build
+    {
+      input: 'src/index.js',
+      external: [], // indicate which modules should be treated as external
+      output: [
+        {
+          banner,
+          file: pkg.main,
+          format: 'cjs'
+        },
+        {
+          banner,
+          file: pkg.module,
+          format: 'es'
+        }
+      ],
+      plugins: [
+        babel({
+          exclude: ['node_modules/**']
+        })
+      ]
+    }
+  ];
+
+  if (commandLineArgs.environment === 'BUILD:production') {
+    // UMD Production
+    configs.push({
+      input: 'src/index.js',
+      output: {
         banner,
-        file: pkg.module,
-        format: 'es'
-      }
-    ],
-    plugins: [
-      babel({
-        exclude: ['node_modules/**']
-      })
-    ]
+        name: libraryName,
+        file: `dist/${libraryName}.umd.min.js`,
+        format: 'umd'
+      },
+      plugins: [
+        // Uncomment the following 2 lines if your library has external dependencies
+        // resolve(), // teach Rollup how to find external modules
+        // commonjs(), // so Rollup can convert external modules to an ES module
+        babel({
+          exclude: ['node_modules/**']
+        }),
+        terser({
+          output: {
+            comments: /^!/
+          }
+        })
+      ]
+    });
   }
-];
+
+  return configs;
+};
